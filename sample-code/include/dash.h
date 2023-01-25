ae33763fad61b07cc6e5aa2cfcc8d7e390cab5a9
Kellen Lai <klai8899@gmail.com>
2023-01-25 17:17:16 -0600

    testerrorlist

--- sample-code/include/dash.h
+++ sample-code/include/dash.h
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include "teensy_can.h"
 #include "virtualTimer.h"
+#include <bits/stdc++.h>
+#include <iostream>
 
 #ifndef __DASH_H__
 #define __DASH_H__
@@ -10,8 +12,20 @@
 class Dash {
 public:
   char mode;
+  int size;
+  int counter;
 
   void Initialize();
+
+  class Node {
+      public:
+    char* msg;
+    Node* next;
+  };
+
+  Node* head;
+  
+
   uint16_t UpdateBackground(uint16_t FWol, uint8_t r, uint8_t g, uint8_t b);
   uint16_t AddToDisplayList(uint16_t FWol);
 private:
@@ -20,10 +34,19 @@ private:
   VirtualTimerGroup timer_group{};
   CANSignal<float, 0, 16, CANTemplateConvertFloat(1), CANTemplateConvertFloat(-40), false> motor_temp_signal;
   CANSignal<float, 0, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0), false> fl_wheel_speed_signal;
-  CANRXMessage<1> rx_ptrain{hp_can_bus, 0x420, motor_temp_signal};
+  CANSignal<float, 0, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0), false> fr_wheel_speed_signal;
+  CANSignal<float, 0, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0), false> bl_wheel_speed_signal;
+  CANSignal<float, 0, 16, CANTemplateConvertFloat(0.1), CANTemplateConvertFloat(0), false> br_wheel_speed_signal;
+  
   CANRXMessage<1> rx_flwheel{lp_can_bus, 0x400, fl_wheel_speed_signal};
-
+  CANRXMessage<1> rx_frwheel{lp_can_bus, 0x401, fr_wheel_speed_signal};
+  CANRXMessage<1> rx_blwheel{lp_can_bus, 0x402, bl_wheel_speed_signal};
+  CANRXMessage<1> rx_brwheel{lp_can_bus, 0x403, br_wheel_speed_signal};
+  CANRXMessage<1> rx_ptrain{hp_can_bus, 0x420, motor_temp_signal};
   void GetCAN();
+  void UpdateErrorList();
+  void ErrorListAdd();
+  void ErrorListRemove();
 };
 
 #endif
--- sample-code/src/dash.cpp
+++ sample-code/src/dash.cpp
@@ -3,6 +3,7 @@
 #include <SPI.h>
 #include <stdarg.h>
 #include <math.h>
+#include <string.h>
 
 #include "CFA10100_defines.h"
 
@@ -26,17 +27,51 @@ void Dash::GetCAN() {
   // radius = 32 + uint16_t(32 * float_signal);
 }
 
+void Dash::ErrorListAdd() {
+
+}
+
+void Dash::ErrorListRemove() {
+
+}
+
+void Dash::UpdateErrorList() {
+  Node* one = new Node();
+  Node* two = new Node();
+
+  one->msg = "Error 1";
+  size = size + 1;
+  two->msg = "Error 2";
+  size = size + 2;
+
+  one->next = two;
+  two->next = NULL;
+
+  head = one;
+
+  // while (head != NULL) {
+  //   head = head->next;
+  // }
+}
+
 void Dash::Initialize() {
   hp_can_bus.Initialize(ICAN::BaudRate::kBaud1M);
   lp_can_bus.Initialize(ICAN::BaudRate::kBaud1M);
   hp_can_bus.RegisterRXMessage(rx_ptrain);  // Temporary workaround
   lp_can_bus.RegisterRXMessage(rx_flwheel);
+  lp_can_bus.RegisterRXMessage(rx_frwheel);
+  lp_can_bus.RegisterRXMessage(rx_blwheel);
+  lp_can_bus.RegisterRXMessage(rx_brwheel);
 
   // Light or Dark
   mode = 0;
+  size = 0;
+  counter = 0;
 
   timer_group.AddTimer(1, [this]() { this->GetCAN(); });
   timer_group.AddTimer(3000, [this]() { mode = 1 - mode; });
+  timer_group.AddTimer(1000, [this]() { this->UpdateErrorList(); });
+
 }
 
 uint16_t Dash::UpdateBackground(uint16_t FWol, uint8_t r, uint8_t g, uint8_t b) {
@@ -50,10 +85,14 @@ uint16_t Dash::AddToDisplayList(uint16_t FWol) {
     uint8_t(rgb[2] * 255)
   ));
 
-  const float kWheelSpeed = 80.0;
-  const float kMotorTemp = 10.0;
+  // const float kWheelSpeed = 80.0;
+  // const float kMotorTemp = 10.0;
   float fl_wheel_speed = static_cast<float>(fl_wheel_speed_signal);
+  float fr_wheel_speed = static_cast<float>(fr_wheel_speed_signal);
+  float bl_wheel_speed = static_cast<float>(bl_wheel_speed_signal);
+  float br_wheel_speed = static_cast<float>(br_wheel_speed_signal);
   float motor_temp = static_cast<float>(motor_temp_signal);
+  float wheel_speed_avg = static_cast<float>((fl_wheel_speed + fr_wheel_speed + bl_wheel_speed + br_wheel_speed) / 4.0);
 
   // Draw a circle in the center of the screen 800 x 480
   // FWol = EVE_Point(
@@ -62,19 +101,47 @@ uint16_t Dash::AddToDisplayList(uint16_t FWol) {
   //   LCD_HEIGHT * 8,
   //   radius * 8
   // );
-  FWol = EVE_Filled_Rectangle(
-    FWol,
-    20,
-    (LCD_HEIGHT / 2) - 10,
-    uint16_t(0 * (LCD_WIDTH - 50)) + 30,
-    (LCD_HEIGHT / 2) + 10
-  );
+  // Draw a blue rectangle on the left side of the screen
+  // FWol = EVE_Filled_Rectangle(
+  //   FWol,
+  //   20,
+  //   (LCD_HEIGHT / 2) - 10,
+  //   uint16_t(0 * (LCD_WIDTH - 50)) + 30,
+  //   (LCD_HEIGHT / 2) + 10
+  // );
+
+  // String s = "this is a longer string";
+  // const char* str = msg[i].c_str();
+  
+  // Node* head;
+
+  // Node* one = new Node();
+  // Node* two = new Node();
+
+  // one->msg = "Error 1";
+  // size = size + 1;
+  // two->msg = "Error 2";
+  // size = size + 2;
+
+  // one->next = two;
+  // two->next = NULL;
+
+  // head = one;
 
   FWol = EVE_Cmd_Dat_0(FWol, EVE_ENC_COLOR_RGB(
     uint8_t(mode * 255),
     uint8_t(mode * 255),
     uint8_t(mode * 255)
   ));
+  FWol = EVE_PrintF(
+    FWol,
+    LCD_WIDTH / 2,
+    (LCD_HEIGHT / 2) - 84,
+    25,
+    EVE_OPT_CENTER,
+    "Error Message: %s",
+    head->msg
+  );
   FWol = EVE_PrintF(
     FWol,
     LCD_WIDTH / 2,
@@ -91,14 +158,17 @@ uint16_t Dash::AddToDisplayList(uint16_t FWol) {
     25,
     EVE_OPT_CENTER,
     "Wheel speed signal: %f",
-    fl_wheel_speed
+    wheel_speed_avg
   );
 
   Serial.print("Motor temp: ");
   Serial.print(motor_temp_signal);
   Serial.print("\n");
-  Serial.print("FL wheel speed: ");
-  Serial.print(fl_wheel_speed_signal);
+  Serial.print("Avg wheel speed: ");
+  Serial.print(wheel_speed_avg);
+  Serial.print("\n");
+  Serial.print("Error Message: ");
+  //Serial.print();
   Serial.print("\n");
 
   timer_group.Tick(millis());
